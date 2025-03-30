319485838767e226c50201e3a5e8b95f6b23a10e
Margo Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-03-30 03:19:08 +0000

    some more exercises

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
 
-let purchaseEl = document.getElementById("purchase-btn")
-let errorEl = document.getElementById("error")
+// Create four functions: add(), subtract(), divide(), multiply()
 
-function writeError() {
-    console.log("Something went wrong, please try again")
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    result = num1 + num2
+    sumEl.textContent = "Sum: " + result
 }
+
+function subtract() {
+    result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
+
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
