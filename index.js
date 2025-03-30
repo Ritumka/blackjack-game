32f00d0cdda43fe9f8ccecaa6608a605fd9b775d
Margo Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-03-30 19:26:20 +0000

    started

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-// Create four functions: add(), subtract(), divide(), multiply()
-
+let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-function add() {
-    result = num1 + num2
-    sumEl.textContent = "Sum: " + result
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " and " + secondCard
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-
-function subtract() {
-    result = num1 - num2
-    sumEl.textContent = "Sum: " + result
-}
-
-function divide() {
-    result = num1 / num2
-    sumEl.textContent = "Sum: " + result
-}
-
-function multiply() {
-    result = num1 * num2
-    sumEl.textContent = "Sum: " + result
-}
-
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
